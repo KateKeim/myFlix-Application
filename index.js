76497df47cc0b5cc89e007a323c50b9eb7b58951
KateKeim <110600575+KateKeim@users.noreply.github.com>
2023-02-21 14:31:50 +0100

    Add Top 10 movies

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express"),
+  morgan = require("morgan"),
+  fs = require("fs"),
+  path = require("path"),
+  movies = require("./movies.json");
+
+const app = express();
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+  flags: "a",
+});
+
+//setiing Morgan
+app.use(morgan("combined", { stream: accessLogStream }));
+app.use(express.static("public"));
+
+//Morgan middleware error handling function
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Error");
+});
+
+//GET request
+app.get("/movies", (req, res) => {
+  res.json(movies);
+});
+app.get("/", (req, res) => {
+  res.send("This is my first Node Application.");
+});
+
+//Listening request post 8080
+app.listen(8080, () => {
+  console.log("Website started on port 8080");
+});
