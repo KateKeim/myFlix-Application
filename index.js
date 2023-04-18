b957ef9efcfbd9416e3de557fb788d0c75a14e19
KateKeim <110600575+KateKeim@users.noreply.github.com>
2023-04-18 20:35:09 +0200

    update cors

--- index.js
+++ index.js
@@ -29,18 +29,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //CORS setting 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflixck.netlify.app'];
+//let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflixck.netlify.app'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+ app.use(cors(
+  //{
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }
+));
 
 //import auth.js file
 let auth = require("./auth")(app);
